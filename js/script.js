caf4f091b4b79c25fa8f5b02bb2c736d7dc6641b
Vuurvos1 <47493725+Vuurvos1@users.noreply.github.com>
2020-10-16 23:37:08 +0200

    setting up package

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+// check device
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  // true for mobile device
+  console.log('mobile device');
+} else {
+  // false for not mobile device
+  console.log('not mobile device');
+}
+
 // let accelerometer = new Accelerometer({
 //   frequency: 1,
 // });
